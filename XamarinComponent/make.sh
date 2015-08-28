23af85f74c9b04c98384b12a47464bc8f6c1ab1f
robert.waggott <rob@earthware.co.uk>
2015-08-28 21:48:23 +0100

    making the build number of the make script a parameter

--- XamarinComponent/make.sh
+++ XamarinComponent/make.sh
@@ -1,1 +1,6 @@
-mono xamarin-component.exe create-manually PlaceholderEnabledUITextView-1.0.xam --name="Placeholder enabled UITextView" --summary="A UITextView extender so you can stop asking why UITextField has a placeholder but UITextView doesn't?!" --publisher="Robert Waggott" --website="https://github.com/robert-waggott/Xamarin.PlaceholderEnabledUITextView" --details="Details.md" --license="License.md" --getting-started="GettingStarted.md" --icon="icons/PlaceholderEnabledUITextView_128x128.png" --icon="icons/PlaceholderEnabledUITextView_512x512.png" --library="ios-unified":"lib/PlaceholderEnabledUITextView.dll" --sample="iOS Sample. Sample of how to use the component in your iOS application.":"samples/Sample.sln"
+#!/bin/bash
+# Call this script with the component's build number
+
+echo "build #$1"
+
+mono xamarin-component.exe create-manually PlaceholderEnabledUITextView-$1.xam --name="Placeholder enabled UITextView" --summary="A UITextView extender so you can stop asking why UITextField has a placeholder but UITextView doesn't?!" --publisher="Robert Waggott" --website="https://github.com/robert-waggott/Xamarin.PlaceholderEnabledUITextView" --details="Details.md" --license="License.md" --getting-started="GettingStarted.md" --icon="icons/PlaceholderEnabledUITextView_128x128.png" --icon="icons/PlaceholderEnabledUITextView_512x512.png" --library="ios-unified":"lib/PlaceholderEnabledUITextView.dll" --sample="iOS Sample. Sample of how to use the component in your iOS application.":"samples/Sample.sln"
